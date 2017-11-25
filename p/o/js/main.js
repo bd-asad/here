8d532960eb95b130b52cef3ac644727d39c3c18b
Md. Asaduzzaman <bd.asad.bd@gmail.com>
2017-11-25 08:31:55 +0600

    portfolio files
    o files

---
+++ p/o/js/main.js
@@ -0,0 +1,11 @@
+    $(document).ready(function() {
+     
+      $("#owl-example").owlCarousel({
+      	singleItem : true,
+      	navigation : false,
+      	pagination : false,
+      	transitionStyle : "fade",
+      	autoPlay : 2000,
+      });
+     
+    });
